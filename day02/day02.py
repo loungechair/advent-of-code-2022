7af365ae4b82c81824f7d80eaea3c34d9234039d
Kevin Durda <lounge.chair@gmail.com>
2022-12-10 16:38:16 -0500

    Refactor Day 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,12 +1,14 @@
+import functools
 from enum import Enum
+from typing import Tuple
 
 FILE = "input.txt"
 
 
 class RPS(Enum):
-    Rock = 1
-    Paper = 2
-    Scissors = 3
+    Rock = 0
+    Paper = 1
+    Scissors = 2
 
 
 class Outcome(Enum):
@@ -15,64 +17,64 @@ class Outcome(Enum):
     Win = 'Z'
 
 
-beats = {
-    RPS.Rock: RPS.Scissors,
-    RPS.Paper: RPS.Rock,
-    RPS.Scissors: RPS.Paper
-}
+def what_beats(shape: RPS) -> RPS:
+    return RPS((shape.value + 1) % 3)
 
 
-def p1_lookup(v):
-    return RPS(1 + ord(v) - ord('A'))
+def what_loses_to(shape: RPS) -> RPS:
+    return RPS((shape.value + 2) % 3)
 
 
-def p2_lookup(v):
-    return RPS(1 + ord(v) - ord('X'))
+def lookup(v: str, offset: str) -> RPS:
+    return RPS(ord(v) - ord(offset))
 
 
-def score_rps_part(p1, p2):
+p1_lookup = functools.partial(lookup, offset='A')
+p2_lookup = functools.partial(lookup, offset='X')
+
+
+def score_rps_part(p1: RPS, p2: RPS) -> int:
     """
     Determines if p2 beats p1
     """
     if p1 == p2:
-        return 3  # draw
-    if p1 == beats[p2]:
+        return 3
+    elif p2 == what_beats(p1):
         return 6
     else:
         return 0
 
 
-def score_game(p1, p2):
-    return p2.value + score_rps_part(p1, p2)
+def score_game(p1: RPS, p2: RPS) -> int:
+    return 1 + p2.value + score_rps_part(p1, p2)
 
 
-def pairs_generator(filename):
+def pairs_generator(filename: str) -> Tuple[str, str]:
     with open(filename) as f:
         for line in f:
             p1, p2 = line.split()
             yield p1, p2
 
 
-def get_total_score(filename):
-    sum = 0
+def get_total_score(filename: str) -> int:
+    total_score = 0
     for p1, p2 in pairs_generator(filename):
         p1 = p1_lookup(p1)
         p2 = p2_lookup(p2)
-        sum += score_game(p1, p2)
+        total_score += score_game(p1, p2)
+    return total_score
 
-    return sum
 
-
-def find_right_shape(p1, outcome):
+def find_right_shape(p1: RPS, outcome: Outcome) -> RPS:
     if outcome == Outcome.Draw:
         return p1
     elif outcome == Outcome.Lose:
-        return beats[p1]
+        return what_loses_to(p1)
     else:
-        return beats[beats[p1]]
+        return what_beats(p1)
 
 
-def play_second_game(filename):
+def play_second_game(filename: str) -> int:
     total_score = 0
     for p1, outcome in pairs_generator(filename):
         p1 = p1_lookup(p1)
